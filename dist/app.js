600420b0bf1b8482f7356b7244fe50fb3a30f239
Roque <rorts@intrasoft-intl.com>
2020-08-19 21:25:31 +0200

    First commit

--- dist/app.js
+++ dist/app.js
@@ -1,3 +1,9 @@
+"use strict";
+
+var _sha = _interopRequireDefault(require("crypto-js/sha256"));
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
+
 /**
  * Step 1: Verify 'crypto-js' module is installed for your project
  * After verifying it is installed, move on to Step 2. No code needs to be written here.
@@ -7,20 +13,15 @@
  * Step 2: Import from crypto-js module the sha265 library
  */
 
-
-import sha256 from 'crypto-js/sha256';
-
 /**
  * Variables: Do not change variable values to be able to answer the quiz
  */
-
-const data1 = "Blockchain Rock!";
-const dataObject = {
-	id: 1,
-	body: "With Object Works too",
-	time: new Date().getTime().toString().slice(0, -3)
+var data1 = "Blockchain Rock!";
+var dataObject = {
+  id: 1,
+  body: "With Object Works too",
+  time: new Date().getTime().toString().slice(0, -3)
 };
-
 /**
  * Step 3: Add code to the `generate hash function
  * Function that generate the SHA256 Hash
@@ -28,13 +29,12 @@ const dataObject = {
  */
 
 function generateHash(obj) {
-	return sha256(JSON.stringify(obj));
+  return (0, _sha["default"])(JSON.stringify(obj));
 }
 
-console.log(`SHA256 Hash: ${generateHash(data1)}`);
+console.log("SHA256 Hash: ".concat(generateHash(data1)));
 console.log("************************************");
-console.log(`SHA256 Hash: ${generateHash(dataObject)}`);
-
+console.log("SHA256 Hash: ".concat(generateHash(dataObject)));
 /**
  * Run your application using `node app.js`
  */
